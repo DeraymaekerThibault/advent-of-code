70bc52083f283bdae113ae316d12288bffc827ef
DeraymaekerThibault <thibault.deraymaeker@gmail.com>
2026-05-05 15:07:16 +0200

    Add initial setup for 2025 solutions with Cargo.toml and main.rs files

---
+++ 2025/05/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let input = include_str!("../input.txt");
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
+}
+
+fn part1(input: &str) -> &str {
+    todo!()
+}
+
+fn part2(input: &str) -> &str {
+    todo!()
+}
